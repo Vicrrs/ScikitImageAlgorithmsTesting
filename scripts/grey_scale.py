782d1624c6cf2693e6b99064cb7e22db9ba27c18
Vicrrs <enricofermi2019@gmail.com>
2023-06-03 02:19:57 -0400

    grey scale

---
+++ scripts/grey_scale.py
@@ -0,0 +1,9 @@
+from skimage import io, color
+from matplotlib import pyplot as plt
+
+img = io.imread(r"imgs\img014.jpg")
+
+gray_img = color.rgb2gray(img)
+
+plt.imshow(gray_img, cmap="gray")
+plt.show()
