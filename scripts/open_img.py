b009838fef594c125873360bab834fa37da9a32c
Vicrrs <enricofermi2019@gmail.com>
2023-06-03 02:15:37 -0400

    refactor

--- scripts/open_img.py
+++ scripts/open_img.py
@@ -1,12 +1,9 @@
-import os
-import skimage
 from skimage import io
 from matplotlib import pyplot as plt
 
-file_name = os.path.join(
-    skimage.data_dir,
-    r"C:\Users\rozas\Projects\ScikitImageAlgorithmsTesting\imgs\img04.png")
-lena = io.imread(file_name)
+# loading image
+img = io.imread(r"imgs\img02.png")
 
-plt.imshow(lena)
+# displaying the image
+plt.imshow(img)
 plt.show()
