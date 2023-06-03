08647e8b2499fd9899b5ea4640e02f2f5cf16e5f
Vicrrs <enricofermi2019@gmail.com>
2023-06-03 02:20:57 -0400

    Convert to grayscale

--- scripts/grey_scale.py
+++ scripts/grey_scale.py
@@ -2,7 +2,7 @@ from skimage import io, color
 from matplotlib import pyplot as plt
 
 img = io.imread(r"imgs\img014.jpg")
-
+# Convert to grayscale
 gray_img = color.rgb2gray(img)
 
 plt.imshow(gray_img, cmap="gray")
